d20b4e43a714908bfb732ef4720d8cde2b0daca0
chixin <chixin@bonc.com.cn>
2021-07-25 10:50:51 +0800

    第二次交作业

--- gk_homework_2/src/main/java/hadoop_rpc.java
+++ gk_homework_2/src/main/java/hadoop_rpc.java
@@ -9,7 +9,7 @@ public class hadoop_rpc {
     public static void main(String[] args) {
         try {
             MyInterface proxy = RPC.getProxy(MyInterface.class, 1L, new InetSocketAddress("127.0.0.1", 12345), new Configuration());
-            String res = proxy.findName(20190389010002L);
+            String res = proxy.findName(20210123456799L);
             System.out.println(res);
         }catch (IOException e){
             e.printStackTrace();
